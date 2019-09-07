9f07ce769e32fd57f03034fbff923436e2154eee
Nick Siderakis <nixsid@gmail.com>
2019-09-07 12:00:22 -0400

    Update PagerAndroid.js

--- src/PagerAndroid.js
+++ src/PagerAndroid.js
@@ -4,10 +4,10 @@ import * as React from 'react';
 import { View, StyleSheet, I18nManager } from 'react-native';
 import { PagerRendererPropType } from './PropTypes';
 import type { PagerRendererProps } from './TypeDefinitions';
-import { ViewPagerAndroid } from '@react-native-community/viewpager';
+import ViewPager  from '@react-native-community/viewpager';
 import { createNativeWrapper } from 'react-native-gesture-handler';
 
-const WrappedViewPagerAndroid = createNativeWrapper(ViewPagerAndroid, {
+const WrappedViewPager= createNativeWrapper(ViewPager, {
   disallowInterruption: true,
 });
 type PageScrollEvent = {
@@ -162,7 +162,7 @@ export default class PagerAndroid<T: *> extends React.Component<Props<T>> {
     const initialPage = this._getPageIndex(navigationState.index);
 
     return (
-      <WrappedViewPagerAndroid
+      <WrappedViewPager
         key={navigationState.routes.length}
         keyboardDismissMode={keyboardDismissMode}
         initialPage={initialPage}
@@ -174,7 +174,7 @@ export default class PagerAndroid<T: *> extends React.Component<Props<T>> {
         ref={el => (this._viewPager = el)}
       >
         {content}
-      </WrappedViewPagerAndroid>
+      </WrappedViewPager>
     );
   }
 }
